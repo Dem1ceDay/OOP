8e235ff601a5f7763eff8144f68b79a1a5736685
Tigerman6006 <thanksforyourtimeuuu@gmail.com>
2026-03-05 08:50:25 +0300

    fourth

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ string Articul = "123123232212",
 Name = "Шубка Норковая",
 Price = "110000",
 Category = "Верхняя одежда";
-int Volume = 7000,	amount = 2000;
+int Volume = 7000;
 
 Product SHUBA = new Product(Articul, Name, Price, Category, Volume);
 SHUBA.Print();
